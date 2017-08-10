ebca14f8b1b1edc0c4662b09337086886a9a7e0e
Youssef Ehab <youssefehab.cce@gmail.com>
2017-08-11 00:03:07 +0200

    cors

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var cors = require('cors');
 var mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
+app.use(CORS.cors);
 mongoose.connect(process.env.MONGOLAB_URI);
 app.set('superSecret','joe-productions');
 
