fd53e4527260bb1a693d226fab32f5e6487a42b4
Youssef Ehab <youssefehab.cce@gmail.com>
2017-08-11 11:36:04 +0200

    cors

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var mongoose = require('mongoose');
 //app.options('*', cors());
 
 //app.use(cors());
-app.all('*', function(req, res, next) {
-     res.header('Access-Control-Allow-Origin', '*');
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use('*',function(req, res, next) {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
      next();
 });
 app.use(bodyParser.urlencoded({ extended: true }))
