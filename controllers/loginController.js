5911ed44f1587d92d6eab431a92c091e6e5e5939
Youssef Ehab <youssefehab.cce@gmail.com>
2017-08-11 19:30:00 +0200

    update

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -10,6 +10,7 @@ module.exports.login = function(req,res,next){
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+            res.setHeader('Content-Type','application/json');
             if (!user){
                 res.json({
                     message: "Authentication failed. User not found."
