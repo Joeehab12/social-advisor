adc18dd8e264de0f023d9cb833567a93c2ee03bb
Youssef Ehab <youssefehab.cce@gmail.com>
2017-08-11 04:24:54 +0200

    cors

--- social-advisor-frontend/server.js
+++ social-advisor-frontend/server.js
@@ -14,8 +14,6 @@ var mongoose = require('mongoose');
       next();
     }
 };*/
-app.use(cors());
-
 app.set('view engine','ejs');
 app.set('views',__dirname + '/public/views');
 
@@ -28,32 +26,26 @@ mongoose.connect(process.env.MONGOLAB_URI);
 app.set('superSecret','joe-productions');
 
 app.get('/',function(req,res,next){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.render('index');
 });
 
 app.get('/login',function(req,res,next){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.render('login');
 });
 
 app.get('/register',function(req,res,next){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.render('register');
 });
 
 app.get('/feed',function(req,res,next){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.render('feed');
 });
 
 app.get('/ask',function(req,res,next){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.render('ask');
 });
 
 app.get('/problem/:id',function(req,res,next){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.render('problem');
 });
 
