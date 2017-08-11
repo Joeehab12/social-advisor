9f951f4dfc4ddbed2644269301e0fcd148e23477
Youssef Ehab <youssefehab.cce@gmail.com>
2017-08-11 21:35:04 +0200

    blabla

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -8,15 +8,13 @@ module.exports.register = function (req,res,next){
         password: req.body.password
     });
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'PATCH, DELETE, POST, GET, OPTIONS');
+    res.setheader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     new_user.save(function(err){
         if (err) throw err;
         console.log('user saved successfuly');
-
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'PATCH, DELETE, POST, GET, OPTIONS');
-        res.setheader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
         res.json({
             status: "success"
         });
--- social-advisor-frontend/public/scripts/register.js
+++ social-advisor-frontend/public/scripts/register.js
@@ -12,4 +12,23 @@ $(document).ready(function(){
             }
         });
     });
+
+
+    $("#password").keypress(function(event){
+        if (event.which == 13){
+            $.post('https://social-advisor-heroku.herokuapp.com/register',
+            { firstName: $('#first_name').val(),
+              lastName: $('#last_name').val(),
+              username: $('#username').val(),
+              password: $('#password').val()
+            })
+            .done(function(data){
+                if (data.status == "success"){
+                        console.log('user saved successfuly');
+                }
+            });
+        }
+    });
+
+
 });
