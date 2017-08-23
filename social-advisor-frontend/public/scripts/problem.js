cb66e13f9d8c8d173a46e9c56f2a677c740a4ac2
Youssef Ehab <you@example.com>
2017-08-23 07:49:24 +0200

    update

--- social-advisor-frontend/public/scripts/problem.js
+++ social-advisor-frontend/public/scripts/problem.js
@@ -2,7 +2,7 @@ var url = $(location).attr('href');
 var id = url.substring(url.lastIndexOf('/') + 1);
 //console.log(id);
 $(document).ready(function(){
-        $.post('http://localhost:8000/problem/'+id +'?' + $.cookie("token"),null)
+        $.post('http://localhost:8000/problem/'+id +'?token=' + $.cookie("token"),null)
         .done(function(data){
                 console.log(data);
                 $('#title').text(data.title);
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 
             $('#comment').click(function(){
-                $.post('http://localhost:8000/comment/'+ id +'?' + $.cookie("token"),{comment: $('#comment-textarea').val()})
+                $.post('http://localhost:8000/comment/'+ id +'?token=' + $.cookie("token"),{comment: $('#comment-textarea').val()})
                 .done(function(data){
                     if (data.status == "success"){
                         console.log("comment added successfuly");
@@ -20,7 +20,7 @@ $(document).ready(function(){
                 });
             });
             str = "";
-            $.get('http://localhost:8000/comment/'+ id +'?' + $.cookie("token"),function(data){
+            $.get('http://localhost:8000/comment/'+ id +'?token=' + $.cookie("token"),function(data){
                 console.log(data);
                 var comments = data[0].comments;
                 comments.forEach(function(item){
@@ -29,7 +29,7 @@ $(document).ready(function(){
                 $('#comments').append(str);
             });
 
-            $.get('http://localhost:8000/answers/'+ id +'?' + $.cookie("token"),function(data){
+            $.get('http://localhost:8000/answers/'+ id +'?token=' + $.cookie("token"),function(data){
                 str = "";
                 console.log(data);
                 var answers = data.answers;
@@ -40,7 +40,7 @@ $(document).ready(function(){
             })
 
             $('#answer').click(function(){
-                $.post('http://localhost:8000/answers/'+ id +'?' + $.cookie("token"),{answer: $('#answer-textarea').val()})
+                $.post('http://localhost:8000/answers/'+ id +'?token=' + $.cookie("token"),{answer: $('#answer-textarea').val()})
                 .done(function(data){
                     if (data.status == "success"){
                         console.log("answer added successfuly");
