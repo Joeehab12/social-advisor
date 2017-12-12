745ead734ccb16a3491be69cb164d9d7003d0e03
Joeehab12 <youssefehab.cce@gmail.com>
2017-12-12 09:29:00 +0200

    added upvote in frontend

--- social-advisor-frontend/public/scripts/problem.js
+++ social-advisor-frontend/public/scripts/problem.js
@@ -21,22 +21,40 @@ $(document).ready(function(){
                         $('#comment-textarea').val('');
                     }
                 });
+                //refresh comments on click
+                var comments1 = "";
+                var userCommentIds=[];
+                $.get('http://localhost:8000/comment/'+ id +'?token=' + $.cookie("token"),function(data){
+                    $("#comments").empty();
+                    console.log(data);
+                    var i=1;
+                    var comments = data[0].comments;
+                    comments.forEach(function(item){
+                  // place span here
+                        userCommentIds.push(item.user_id);
+                        comments1+= '<span> <img id="comment_pic'+i+'" width = "30px" height = "30px"></img></span>';
+                        comments1+= '<p>' + item.body + '</p>';
+                        i++;
+                    });
+                    $('#comments').append(comments1);
+                });
+
             });
-            str = "";
+            var comments2 = "";
             var userCommentIds=[];
             $.get('http://localhost:8000/comment/'+ id +'?token=' + $.cookie("token"),function(data){
-
+                $("#commments").empty();
                 console.log(data);
                 var i=1;
                 var comments = data[0].comments;
                 comments.forEach(function(item){
               // place span here
                     userCommentIds.push(item.user_id);
-                    str+= '<span> <img id="comment_pic'+i+'" width = "30px" height = "30px"></img></span>';
-                    str+= '<p>' + item.body + '</p><br>';
+                    comments2+= '<span> <img id="comment_pic'+i+'" width = "30px" height = "30px"></img></span>';
+                    comments2+= '<p>' + item.body + '</p>';
                     i++;
                 });
-                $('#comments').append(str);
+                $('#comments').append(comments2);
             });
 
 
@@ -46,13 +64,32 @@ $(document).ready(function(){
 
 
             $.get('http://localhost:8000/answers/'+ id +'?token=' + $.cookie("token"),function(data){
-                str = "";
+                var answers1 = "";
                 console.log(data);
+                $("#answers").empty();
                 var answers = data.answers;
+                console.log(answers);
+                var i = 1;
                 answers.forEach(function(item){
-                    str+= '<p>' + item.body + '</p><br>';
+                    if (item.body != ""){
+                    answers1 += '<div id = \"answerContainer\">'
+                    answers1+= '<div id=\"topic'+i+'\" class=\"upvote\">'+'<a class=\"upvote\"></a>'+
+                    '<span class=\"count\">0</span>'+
+                        '<a class=\"downvote\"></a>'
+                        + '<a class = \"star\"> </a>' +
+                    '</div>';
+                    answers1+= '<p>' + item.body + '</p><br>';
+                    console.log(answers1);
+                    answers1+= '</div>'
+                    $('#answers').append(answers1);
+                    answers1 = "";
+                    i++;
+                    }
                 });
-                $('#answers').append(str);
+                for (var j = 1;j<=i;j++){
+                    $("#topic" +j).upvote();
+                }
+
             })
 
             $('#answer').click(function(){
@@ -63,6 +100,34 @@ $(document).ready(function(){
                         $('#answer-textarea').val('');
                     }
                 });
+
+                $.get('http://localhost:8000/answers/'+ id +'?token=' + $.cookie("token"),function(data){
+                    var answers2 = "";
+                    console.log(data);
+                    $("#answers").empty();
+                    var answers = data.answers;
+                    var index = 1;
+                    answers.forEach(function(item){
+                        if (item.body != ""){
+                        answers2 += '<div id = \"answerContainer\">'
+                        answers2+= '<div id=\"topic'+index+'\" class=\"upvote\">'+'<a class=\"upvote\"></a>'+
+                        '<span class=\"count\">0</span>'+
+                            '<a class=\"downvote\"></a>'
+                            + '<a class = \"star\"> </a>' +
+                        '</div>';
+                        answers2+= '<p>' + item.body + '</p>';
+                        answers2+= '</div>';
+                        $('#answers').append(answers2);
+                        $("#topic2").upvote();
+                        answers2 = "";
+                        index++;
+                    }
+                });
+                for (var k = 1;k<=index;k++){
+                    $("#topic" +k).upvote();
+                }
+            });
+
             });
             var i = 1;
             userCommentIds.forEach(function(item){
